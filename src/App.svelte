7b21839fb86f3a1f2d7870af32b8453dfe03f9aa
ShigeyukiYamanaka <yamanaka@zero-spec.com>
2022-06-09 23:38:28 +0900

    sve

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
+	import Header from './components/Header.svelte'
 </script>
 
+<Header/>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>がんばります
-		
-	</p>
+	<h1>中心確認</h1>
 </main>
 
 <style>
-	main {
+	main  {
 		
-		background-color: blue;
-		width: 50%;
 		text-align: center;
-		padding: 1em;
-		/* max-width: 240px; */
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		margin-left:200px;
 	}
 
 	@media (min-width: 640px) {
---
+++ src/components/Header.svelte
@@ -0,0 +1,79 @@
+<script>
+  
+</script>
+
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<header>
+
+  <div class="header-logo">
+    <p>YAMANAKA <br>APP</p>
+  </div>
+
+  <div class="menu-list">
+    <ul>
+      <li><a href="/"><span class="material-icons">home</span> HOME</a></li>
+      <li><a href="/"><span class="material-icons">cloud</span> Coming Soon</a></li>
+      <li><a href="/"><span class="material-icons">cloud</span> Coming Soon</a></li>
+      <li><a href="/"><span class="material-icons">cloud</span> Coming Soon</a></li>
+      <li><a href="/"><span class="material-icons">cloud</span> Coming Soon</a></li>
+    </ul>
+  </div>
+  <span class="material-icons menu-icon">menu</span>
+
+</header>
+
+
+<style>
+  header {
+  background-color: #0bb582;
+  width: 200px;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 80px 0px 40px;
+  box-sizing: border-box;
+}
+
+.header-logo {
+  text-align: center;
+}
+
+
+p{
+  color: #ffffff;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 24px;
+}
+
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+.menu-list a {
+  color: #ffffff;
+  display: block;
+  padding: 16px 24px;
+  font-size: 18px;
+}
+
+.menu-list a:hover {
+  background-color: #11ce66;
+}
+.material-icons{
+  font-size: 15px;
+}
+
+.menu-icon{
+  display: none;
+}
+
+</style>
